29145c999e850d43d3ac093057be567dec29b52b
tuy vo <vthtuy@gmail.com>
2019-03-19 17:57:29 -0500

    call REST

--- csvcollector/src/main/java/dev/cstv/collector/service/impl/SongServiceImpl.java
+++ csvcollector/src/main/java/dev/cstv/collector/service/impl/SongServiceImpl.java
@@ -1,18 +1,73 @@
 package dev.cstv.collector.service.impl;
 
-import org.apache.log4j.Logger;
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import dev.cstv.collector.rest.RestHttpHeader;
 import dev.cstv.collector.service.SongService;
 import dev.cstv.musify.domain.Song;
 
+@Component
 public class SongServiceImpl implements SongService {
 
-	private Logger LOG = Logger.getLogger(SongServiceImpl.class);
+	private Logger LOG = Logger.getLogger(SongServiceImpl.class.getName());
+
+	private static String BASE_URL = "http://dummy.restapiexample.com/api/v1/";
+
+	String baseUrl = "http://localhost:8080/JerseyRestSecurity/songs";
+	String baseUrlExtended = baseUrl + "/";
+
+	@Autowired
+	RestHttpHeader restHttpHeader;
 	
-	public void create(Song song) {
-		// TODO call webservice 
-		System.out.println("calling REST: " + song.getTitle());
-		
+
+	public static void main(String[] args) {
 	}
-	
+
+	public Song read(Integer index) {
+		RestTemplate restTemplate = restHttpHeader.getRestTemplate();
+		return (restTemplate
+				.exchange(baseUrlExtended + index, HttpMethod.GET, restHttpHeader.getHttpEntity(), Song.class)
+				.getBody());
+		// Returns Song in Body HTTP Message
+	}
+
+	public void create(Song song) {
+		// TODO call webservice
+		System.out.println("calling REST: " + song.getTitle());
+		RestTemplate restTemplate = restHttpHeader.getRestTemplate();
+		// HTTPEntity - SEND Headers & Body
+		HttpEntity<Song> httpEntity = new HttpEntity<Song>(song, restHttpHeader.getHttpHeaders());
+		restTemplate.postForObject(baseUrl, httpEntity, Song.class);
+		// Song.class - Response type
+	}
+
+	private static URI getBaseURI() {
+		return UriBuilder.fromUri(BASE_URL).build();
+	}
+
+	public List<Song> read() {
+
+		RestTemplate restTemplate = restHttpHeader.getRestTemplate();
+		return Arrays.asList(
+				restTemplate.exchange(baseUrl, HttpMethod.GET, restHttpHeader.getHttpEntity(), Song[].class).getBody());
+		// restHttpHeader.getHttpEntity() - get HTTP headers [Authentication; JSON
+		// ACCEPT]
+	}
+
 }
--- csvcollector/src/main/java/dev/cstv/main/Main.java
+++ csvcollector/src/main/java/dev/cstv/main/Main.java
@@ -1,6 +1,5 @@
 package dev.cstv.main;
 
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,29 +10,36 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 import dev.cstv.collector.batch.CSVCollectorBatch;
+import dev.cstv.collector.service.SongService;
+import dev.cstv.collector.service.impl.SongServiceImpl;
 
-
-@Component 
+@Component
 public class Main {
- 
+
 	@Autowired
 	CSVCollectorBatch cSVCollectorBatch;
-	
-  public static void main(String[] args) throws Throwable {
 
-    ApplicationContext context = new ClassPathXmlApplicationContext("context/applicationContext.xml",
-    		"context/batch-config.xml","context/collector-job.xml");
-    	context.getBean(Main.class).mainInternal(context);
-  	}
-  
-    private void mainInternal(ApplicationContext applicationContext) throws Exception {
- 
-	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-	Date date = new Date();
-	System.out.println("Starting System " + dateFormat.format(date));  
-   
-	// This "manual mode" ...ProductBatch will be SCHEDULED if @Scheduled is on ProductBatch Class
-	cSVCollectorBatch.startjob();
-	
-  }
- }
+	public static void main(String[] args) throws Throwable {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("context/applicationContext.xml",
+				"context/batch-config.xml", "context/collector-job.xml");
+		context.getBean(Main.class).mainInternal(context);
+	}
+
+	private void mainInternal(ApplicationContext applicationContext) throws Exception {
+
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		System.out.println("Starting System " + dateFormat.format(date));
+
+		// This "manual mode" ...ProductBatch will be SCHEDULED if @Scheduled is on
+		// ProductBatch Class
+	 	cSVCollectorBatch.startjob();
+
+		SongService  impl = applicationContext.getBean(SongService.class);
+		
+		System.out.println("Data Get From REST !!!");
+		System.out.println(impl.read(1).getTitle());
+
+	}
+}
